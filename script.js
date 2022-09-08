8c3a9037bb1030c6a131dcac4f025f58fad58f92
Prathyusha-Guduru <prathyushaguduru28@gmail.com>
2022-09-08 15:03:57 +0530

    Add: hover effects

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ $(function () {
 firstPoint = document.getElementById("first-point");
 thirdPoint = document.getElementById("third-point");
 
-boxy = document.getElementById("boxy");
 newspaper = document.querySelector(".newspaper");
+mars = document.querySelector(".mars");
 
 console.log(firstPoint);
 
@@ -19,11 +19,16 @@ newspaper.addEventListener("mouseout", () => {
   newspaper.classList.toggle("show");
 });
 
-// thirdPoint.addEventListener("mouseover",())
-
-boxy.addEventListener("mouseover", () => {
-  console.log("YES");
+thirdPoint.addEventListener("mouseover", () => {
+  mars.classList.toggle("show");
+  console.log("thisrd point");
 });
+
+mars.addEventListener("mouseout", () => {
+  mars.classList.toggle("show");
+});
+
+// thirdPoint.addEventListener("mouseover",())
 
 // firstPoint.addEventListener("click", () => {
 //   console.log("YES");
