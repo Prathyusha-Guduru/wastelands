91d7804fd514c7dd3991a514881c9a9c29623d1e
Prathyusha-Guduru <prathyushaguduru28@gmail.com>
2022-09-11 00:05:09 +0530

    1. Bag sticky postion whitespace error, couldnt fix, 2. Add radio music and made it work on hover

--- script.js
+++ script.js
@@ -5,14 +5,24 @@ $(function () {
 
 firstPoint = document.getElementById("first-point");
 thirdPoint = document.getElementById("third-point");
+secondPoint = document.getElementById("second-point");
 
 newspaper = document.querySelector(".newspaper");
 mars = document.querySelector(".mars");
+audio = document.querySelector(".radio");
 
 console.log(firstPoint);
 
 firstPoint.addEventListener("mouseover", () => {
   newspaper.classList.toggle("show");
+});
+
+secondPoint.addEventListener("mouseover", () => {
+  audio.play();
+});
+
+secondPoint.addEventListener("mouseout", () => {
+  audio.pause();
 });
 
 newspaper.addEventListener("mouseout", () => {
@@ -27,3 +37,40 @@ thirdPoint.addEventListener("mouseover", () => {
 mars.addEventListener("mouseout", () => {
   mars.classList.toggle("show");
 });
+
+// function scrollPos() {
+//   var myDiv = document.querySelector(".team-section").scrollTop;
+//   if (myDiv.offsetHeight + myDiv.scrollTop >= myDiv.scrollHeight) {
+//     console.log("scroll not toppped");
+//   } else {
+//     console.log("scroll is toppped");
+//   }
+// }
+
+// function preventScroll(e) {
+//   e.preventDefault();
+//   e.stopPropagation();
+
+//   return false;
+// }
+
+// window.addEventListener("scroll", (event) => {
+//   console.log(document.querySelector("html").scrollTop);
+
+//   if (document.querySelector("html").scrollTop >= 2429.60009765625) {
+//     console.log("end reached");
+//     // alert("WWRGRG");
+//     // window.addEventListener("DOMMouseScroll", preventDefault, false); // older FF
+//     document
+//       .querySelector("html")
+//       .addEventListener("wheel", preventScroll, { passive: false });
+
+//     // document.querySelector("html").style.overflowY = "hidden";
+//   }
+//   // console.log("Scrolling...");
+// });
+
+// let footer = document.querySelector(".footer");
+// footer.addEventListener("scroll", () => {
+//   console.log("scrollllllllll");
+// });
